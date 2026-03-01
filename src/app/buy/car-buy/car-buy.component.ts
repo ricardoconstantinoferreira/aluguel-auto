5568ccc882b4941a99631b0cf52b2f04b9a41062
Ricardo Constantino Ferreira <ricardo.constantino@engdb.com.br>
2026-03-01 18:54:22 -0300

    create front car with items

---
+++ src/app/buy/car-buy/car-buy.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from 'src/app/auth/auth.service';
+import { BuyService } from '../buy.service';
+
+@Component({
+  selector: 'app-car-buy',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './car-buy.component.html',
+  styleUrl: './car-buy.component.css'
+})
+export class CarBuyComponent implements OnInit {
+
+  allData: any[] = [];
+
+  constructor(private authService: AuthService, private buyService: BuyService) { }
+
+  ngOnInit(): void {
+    this.buyService.getCartItems().subscribe(items => {
+      this.allData = items as any[];
+    }); 
+  }
+
+  cartItemsCount(): number {
+    return this.authService.cartItemsCount;
+  }
+
+  cartItems() {
+    return this.authService.cartItems;
+  }
+
+  removeFromCart(itemId: number): void {
+    this.buyService.removeFromCart(itemId).subscribe(() => {
+      this.allData = this.allData.filter(item => item.model.id !== itemId);
+      let count = localStorage.getItem('cart_items');
+    
+      if (count) {
+        let newCount = parseInt(count) - 1;
+        localStorage.setItem('cart_items', newCount.toString());
+      }
+    });
+  }
+
+}
