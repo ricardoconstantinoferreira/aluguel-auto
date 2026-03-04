a1b96ed704557e9b2feb640c77d42b276018ae35
Ricardo Constantino Ferreira <ricardo.constantino@engdb.com.br>
2026-03-04 00:41:27 -0300

    modal in customer display and looping compoment installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AuthInterceptor } from './auth/auth.interceptor';
+import { NgxLoadingModule } from 'ngx-loading-reloaded-ng19';
 
 @NgModule({
   declarations: [],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxLoadingModule.forRoot({})
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
