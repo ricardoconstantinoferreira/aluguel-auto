b8db3cf760136ebc11901262a75bb489fdacc2b0
Ricardo Constantino Ferreira <ricardo.constantino@engdb.com.br>
2026-03-04 22:26:57 -0300

    fix login problem

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let cloned = req;
@@ -35,6 +36,14 @@ export class AuthInterceptor implements HttpInterceptor {
       });
     }
 
-    return next.handle(cloned);
+    return next.handle(cloned).pipe(
+      catchError(err => {
+        if (err.status === 401 || err.status === 403) {
+          this.auth.logout();
+          this.router.navigate(['/login']);
+        }
+        throw err;
+      })
+    );
   }
 }
