21f36c1ff0381bc3899f6475d417c50ebd629fc3
Ricardo Constantino Ferreira <ricardo.constantino@engdb.com.br>
2026-03-18 16:13:54 -0300

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appBaseUrl: 'https://aluguel-auto.vercel.app/',
+  appBaseUrl: 'https://aluguel-auto.vercel.app',
   apiBasePath: '/api',
   apiBaseUrl: 'https://auto-1-k9gu.onrender.com'
 };
