1dc6e7d7563614185e63b6bb09df737faeb8289e
Ricardo Constantino Ferreira <ricardo.constantino@engdb.com.br>
2026-03-23 13:57:08 -0300

    testing url

--- src/app/clientes/clientes.service.ts
+++ src/app/clientes/clientes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ClientesService {
@@ -8,6 +9,8 @@ export class ClientesService {
 
   create(cliente: any): Observable<any> {
     let uri = 'api/auto/customer';
+    const resolvedUrl = `${environment.apiBaseUrl.replace(/\/+$/, '')}/${uri.replace(/^\/+/, '')}`;
+    console.log('[ClientesService] URL cadastro cliente:', resolvedUrl);
 
     let data = {
       "name": cliente.nome,
