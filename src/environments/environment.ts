c38c4f6f556ecbe708bb790c2d4fceeccd2aa44b
Ricardo Constantino Ferreira <ricardo.constantino@engdb.com.br>
2026-03-23 14:27:39 -0300

    fix environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   appBaseUrl: 'http://localhost:4200',
   apiBasePath: '/api',
-  apiBaseUrl: 'https://auto-1-k9gu.onrender.com'
+  apiBaseUrl: 'http://localhost:8080'
 };
