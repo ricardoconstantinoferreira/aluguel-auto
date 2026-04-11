35b513fa1fb3c9fa0f4fbe93e451b0bc1c41dc62
Ricardo Constantino Ferreira <ricardo.constantino@engdb.com.br>
2026-04-10 22:28:21 -0300

    change environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appBaseUrl: 'https://ferreira-auto.onrender.com',
+  appBaseUrl: 'https://ferreira-auto.com/#/',
   apiBasePath: '/api',
   apiBaseUrl: 'https://auto-1-k9gu.onrender.com'
 };
