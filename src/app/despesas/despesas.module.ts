7b2bb5efe60605a3aaa00883e4f12c367d993e21
Ricardo Constantino Ferreira <ricardo.constantino@engdb.com.br>
2026-04-17 10:22:56 -0300

    creating cadastro and listagem components

--- src/app/despesas/despesas.module.ts
+++ src/app/despesas/despesas.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { TipoDespesaComponent } from './tipo-despesa/tipo-despesa.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 const routes: Routes = [
   { path: 'tipo-despesas', component: TipoDespesaComponent },
-  { path: '', redirectTo: 'tipo-despesas', pathMatch: 'full' }
+  { path: '', redirectTo: 'tipo-despesas', pathMatch: 'full' },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: '', redirectTo: 'cadastro', pathMatch: 'full' },
+  { path: 'listagem', component: ListagemComponent },
+  { path: '', redirectTo: 'listagem', pathMatch: 'full' }
 ];
 
 @NgModule({
---
+++ src/app/despesas/listagem/listagem.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listagem',
+  imports: [],
+  templateUrl: './listagem.component.html',
+  styleUrl: './listagem.component.css'
+})
+export class ListagemComponent {
+
+}
